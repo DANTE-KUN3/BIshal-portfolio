dd4ac74675c884fea7d8c0c2f12fafb9ec9b4a88
DANTE-KUN3 <parikhbishal@gmail.com>
2025-04-04 13:45:35 +0530

    added project info

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,8 +2,9 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-
-
+import todo from "../../Assets/Projects/todo.png";
+import future from "../../Assets/Projects/future.png";
+import dapp from "../../Assets/Projects/dapp.png";
 function Projects() {
   return (
     <Container fluid className="project-section">
@@ -18,7 +19,7 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={chatify}
+              imgPath={future}
               isBlog={false}
               title="Future Self Messaging Platform"
               description="Developed a serverless web application that allows users to schedule and send messages to their future selves via email. Built the backend using AWS Lambda, API Gateway, and DynamoDB for scalability and cost efficiency. The frontend was deployed on Vercel for seamless performance. Implemented AWS Security Groups and VPC to enhance security, ensuring safe data transmission and storage."
@@ -29,7 +30,7 @@ function Projects() {
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={bitsOfCode}
+              imgPath={dapp}
               isBlog={false}
               title="Decentralized Voting Application (DApp)"
               description="Created a secure, blockchain-based voting system using Solidity, Ganache, and Node.js on the Ethereum network. Designed and deployed smart contracts to facilitate tamper-proof voting. Ensured transparency by enabling voters to verify transactions on the blockchain while preventing double voting."
@@ -40,7 +41,7 @@ function Projects() {
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={editor}
+              imgPath={todo}
               isBlog={false}
               title="Task-App"
               description="A simple task management system built with React that allows users to create, edit, complete, and delete tasks. Users can add new tasks through an input field, toggle task completion via checkboxes, and update task titles inline. The UI dynamically updates using React state, providing an interactive experience. The system ensures an efficient task-tracking workflow, helping users stay organized and productive."
@@ -48,6 +49,7 @@ function Projects() {
               demoLink="https://task-app-dun-beta.vercel.app/"              
             />
           </Col>
+
         </Row>
       </Container>
     </Container>
