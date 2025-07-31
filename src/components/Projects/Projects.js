dbff58c0d713111dc511668e7a1c9efdd511ee31
BISHAL <74647844+bishalparikhh@users.noreply.github.com>
2025-07-31 14:26:05 +0530

    Update Projects.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -4,8 +4,6 @@ import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
 import todo from "../../Assets/Projects/todo.png";
 import future from "../../Assets/Projects/future.jpeg";
-import dapp from "../../Assets/Projects/dapp.png";
-import Devops from "../../Assets/Projects/Devops.png";
 import gamers from "../../Assets/Projects/gamers.png";
 
 function Projects() {
@@ -30,28 +28,7 @@ function Projects() {
               demoLink="https://future-mxkq.vercel.app/"
             />
           </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={dapp}
-              isBlog={false}
-              title="Decentralized Voting Application (DApp)"
-              description="Created a secure, blockchain-based voting system using Solidity, Ganache, and Node.js on the Ethereum network. Designed and deployed smart contracts to facilitate tamper-proof voting. Ensured transparency by enabling voters to verify transactions on the blockchain while preventing double voting."
-              ghLink="https://github.com/DANTE-KUN3/voting-dapp"
-              demoLink=""
-            />
-          </Col>
-
-         <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={Devops}
-              isBlog={false}
-              title="DevOps Automation Project"
-              description="Automated the provisioning and configuration of AWS EC2 instances using Terraform and Ansible. Terraform handles the infrastructure setup, while Ansible installs required software and configures instances post-deployment. Integrated with GitHub Actions for seamless automation. Project structure includes modular Terraform files and Ansible playbooks with dynamic inventory support."
-              ghLink="https://github.com/DANTE-KUN3/Devops-Project"
-              demoLink=""              
-            />
-          </Col>
+                
     <Col md={4} className="project-card">
   <ProjectCard
     imgPath={gamers} // Replace with your actual image import
